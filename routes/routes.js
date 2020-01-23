6978950ce80692b9d2ebc22a33b74850af1d5528
Kelly Keating <keating.kel@gmail.com>
2020-01-23 18:40:21 +1300

    router init

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,11 @@
 const express = require('express')
-const router = express
+
+const router = express.Router()
+
 
 router.get('/', (req,res) => {
     res.send("Yoyo")
 })
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 
+const routes = require('./routes/routes')
+
 const server = express()
 
-server.get('/', (req, res) => {
-    res.send(':)')
-})
+server.use('/', routes)
 
 module.exports = server
