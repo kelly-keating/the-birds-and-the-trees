f02876ca376e6aa2b0f3c85c7a0b08372cc95b76
Kelly Keating <keating.kel@gmail.com>
2020-01-23 19:07:28 +1300

    pages init

--- routes/routes.js
+++ routes/routes.js
@@ -9,5 +9,15 @@ router.get('/', (req,res) => {
     res.render('index')
 })
 
+router.get('/:type', (req, res) => {
+    const stuff = data[req.params.type]
+    res.render('list', {thing: stuff})
+})
+
+router.get('/:type/:id', (req, res) => {
+    const theOne = data[req.params.type].find(obj => obj.id == req.params.id)
+    res.render('profile', theOne)
+})
+
 
 module.exports = router
