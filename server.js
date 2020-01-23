5c8f1e562d8d19f63da310578c9b262c554d77fa
Kelly Keating <keating.kel@gmail.com>
2020-01-23 18:45:37 +1300

    hbs init

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express')
-
 const routes = require('./routes/routes')
+
+const express = require('express')
+const hbs = require('express-handlebars')
 
 const server = express()
 
+
+server.engine('hbs', hbs({defaultLayout:'main', extname: 'hbs'}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+
+
 server.use('/', routes)
+
 
 module.exports = server
